233cdfce5788074eb90ea37234c44504884a7177
Jim <chengxuyuan9527@gmail.com>
2022-03-31 22:49:03 +1100

    html grid css

---
+++ app.js
@@ -0,0 +1,15 @@
+//display ending value for x and y
+const lineId = document.querySelector("#line")
+
+lineId.addEventListener("click",() =>{
+    if (lineId.checked) {
+        document.querySelector(".end-value").style.display = "block"
+    }
+})
+
+
+const circleId = document.querySelector("#circle")
+
+
+
+//
